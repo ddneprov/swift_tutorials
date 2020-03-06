c6a714e6e7d31a6cac983684238ba45639651afc
ddneprov <ddneprov_24@mail.ru>
2020-03-07 02:17:49 +0300

    init

---
+++ Task_Tracker/Task_Tracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Task_Tracker
+//
+//  Created by 18177231 on 07/03/2020.
+//  Copyright © 2020 18177231. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
