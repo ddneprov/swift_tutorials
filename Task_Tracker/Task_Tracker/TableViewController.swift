96f397ab09c35d5a166bffc5e43afffc3f86f9c2
ddneprov <ddneprov_24@mail.ru>
2020-03-07 19:54:51 +0300

    final

--- Task_Tracker/Task_Tracker/TableViewController.swift
+++ Task_Tracker/Task_Tracker/TableViewController.swift
@@ -8,22 +8,37 @@
 
 import UIKit
 
-class TableViewController: UITableViewController {
+class TableViewController: UITableViewController, UITextFieldDelegate {
+    
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+           guard let textFieldText = textField.text,
+               let rangeOfTextToReplace = Range(range, in: textFieldText) else {
+                   return false
+           }
+           let substringToReplace = textFieldText[rangeOfTextToReplace]
+           let count = textFieldText.count - substringToReplace.count + string.count
+           return count <= 30
+       }
     
     
     
     @IBAction func PushAddItem(_ sender: Any) {
         let alertController = UIAlertController(title: "Че сделать надо?", message: nil, preferredStyle: .alert)
         
-        alertController.addTextField { (TextField) in
+        
+        alertController.addTextField{ (TextField) in
+            TextField.delegate = self
             TextField.placeholder = "Задача"
         }
+            
+        
+            
         
         alertController.addTextField { (TextField) in
+            TextField.delegate = self
             TextField.placeholder = "А поподробнее?"
         }
-        
-        
         
         let alertAction1 = UIAlertAction(title: "Cancel", style: .default) { (alert) in
         
@@ -40,29 +55,20 @@ class TableViewController: UITableViewController {
         present(alertController, animated: true, completion: nil)
         alertController.addAction(alertAction1)
         alertController.addAction(alertAction2)
-
     }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
-    // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
         return ToDoItems.count
     }
 
@@ -90,22 +96,17 @@ class TableViewController: UITableViewController {
     
 
     
-     //Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-         //Return false if you do not want the specified item to be editable.
         return true
     }
     
 
     
-    // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             removeItem(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        } else if editingStyle == .insert {}
     }
     
     
@@ -150,29 +151,4 @@ class TableViewController: UITableViewController {
         action.backgroundColor = .blue
         return action
     }
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
